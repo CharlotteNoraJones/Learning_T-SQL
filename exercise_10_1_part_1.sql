59f1401810e209cc492215ce656433707000d065
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-02-28 11:12:09 -0600

    Update exercise_10_1_part_1.sql

--- exercise_10_1_part_1.sql
+++ exercise_10_1_part_1.sql
@@ -6,3 +6,15 @@ INSERT INTO dbo.demoSalesOrderHeader
     SubTotal, TaxAmt, Freight)
 SELECT SalesOrderID, OrderDate, CustomerID, SubTotal, TaxAmt, Freight
 FROM Sales.SalesOrderHeader;
+
+SELECT C.CustomerID, 
+    COUNT(ISNULL(SOH.SalesOrderID, 0)) AS TotalOrders,
+    SUM(SOH.TotalDue) AS TotalDue
+INTO dbo.tempCustomerSales
+FROM Sales.Customer AS C
+    LEFT JOIN Sales.SalesOrderHeader AS SOH
+        ON SOH.CustomerID = C.CustomerID
+GROUP BY C.CustomerID;
+
+SELECT TOP (10) CustomerID, TotalOrders, TotalDue
+FROM dbo.tempCustomerSales;
