a3d13be4074d88bd139a1b0902ca2a318f5714ba
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2023-03-23 11:04:10 -0500

    Update exercise_13_4.sql

--- exercise_13_4.sql
+++ exercise_13_4.sql
@@ -22,3 +22,34 @@ DECLARE @Total INT;
 EXEC dbo.usp_ProductSales @ProductID = 776, @TotalSold = @Total OUTPUT;
 PRINT @Total;
 GO
+
+USE WideWorldImporters;
+GO
+
+CREATE OR ALTER PROC dbo.usp_InsertPaymentMethod @Name NVARCHAR(50) AS
+    BEGIN TRAN
+        BEGIN TRY
+            INSERT INTO Application.PaymentMethods (PaymentMethodName, LastEditedBy)
+                VALUES(@Name, 1);
+
+            DECLARE @NewID INT;
+
+            SELECT @NewID = PaymentMethodID
+            FROM Application.PaymentMethods
+            WHERE PaymentMethodName = @Name;
+
+            RETURN @NewID;
+
+            COMMIT TRAN
+        END TRY
+        BEGIN CATCH
+            IF @@TRANCOUNT > 0 BEGIN
+                ROLLBACK TRAN;
+            END;
+
+            SELECT
+                ERROR_NUMBER() AS ErrorNumber,
+                ERROR_MESSAGE() AS ErrorMessage;
+                RETURN -1;
+        END CATCH
+GO
